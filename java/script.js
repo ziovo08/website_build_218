9ec542ba8464eafb67ee6d2f676dc50bf692124c
ziovo08 <zoe.lachica@edu.sait.ca>
2024-04-19 08:56:09 -0600

    container works, needs styling

---
+++ java/script.js
@@ -0,0 +1,26 @@
+/* HEADER CODE */
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+/* HEADER CODE */
+
+
+
+/* MAIN SECTION CODE */
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
